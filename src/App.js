7c91b7d0cc4f651e3b184442ee32a2ca1c3cbe4e
wazihaw <wazihawadud@gmail.com>
2020-08-25 03:48:50 -0400

    creating ui for main screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import TestComponent from './components/TestComponent/TestComponent';
+import SunlocLogo from './components/SunlocLogo/SunlocLogo';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-        		</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React now
-        		</a>
-				<TestComponent number="9" text="Hello World"></TestComponent>
+				<SunlocLogo> </SunlocLogo>
+				<p className="bodyText">what do you want to experience today?</p>
+				<Button className="selectionButton" id="sunriseButton">sunrise</Button>
+				<Button className="selectionButton" id="sunsetButton">sunset</Button>
+				<p className="bodyText" id="locationText">where are you located?</p>
+				<Form className="textInput">
+					<Form.Control placeholder="enter address" />
+				</Form>
 			</header>
 		</div>
 	);
---
+++ src/components/SunlocLogo/SunlocLogo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './SunlocLogo.css'
+
+
+export default function TestComponent() {
+    return(
+        <p id="logo">sunloc</p>
+    )
+}
